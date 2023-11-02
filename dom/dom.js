795d85e9a9b9f11efabfb09e3644a1877215b493
muskanmandal <mandalmuskan12gmail.com>
2023-11-02 13:44:57 +0530

    added dom.html and dom.js in main

---
+++ dom/dom.js
@@ -0,0 +1,17 @@
+// if i want to access 2nd li
+
+const UL = document.querySelector("ul");
+UL.children[1];
+
+// UL.childNodes
+// it selects all child, which means it will count white spaces and text as well
+
+// if we want to access first child of ul
+
+// UL.firstChild
+// it will give us first child which means white space
+
+// UL.firstElementChild
+// it will give us first li of ul
+
+// same as lastChild and lastChildElement
